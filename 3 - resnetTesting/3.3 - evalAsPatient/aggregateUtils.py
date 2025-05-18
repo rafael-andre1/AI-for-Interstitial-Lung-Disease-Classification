b348a7ccd19b4941c0af623ded5dce418080715f
rafa907p <rafael.afp01@gmail.com>
2025-05-18 22:58:57 +0100

    Introduction to Feature Extraction

--- 3 - resnetTesting/3.3 - evalAsPatient/aggregateUtils.py
+++ 3 - resnetTesting/3.3 - evalAsPatient/aggregateUtils.py
@@ -8,7 +8,9 @@ import pandas as pd
 import numpy as np
 import pickle
 import random
+import copy
 import os
+import re
 
 # -----------  ResNet -----------
 
@@ -29,6 +31,458 @@ from torchvision.transforms import ToTensor
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 from torchvision.io import decode_image
+
+
+# ------------------------- Main Class ------------------------- #
+
+class FibrosisDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None, albumentations=None, gauss=False):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+        self.albumentations = albumentations
+        self.gauss = gauss
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        # idx represents index
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        slice_id = self.img_labels.iloc[idx, 0]
+        patient_id = getPatientID(slice_id)
+
+        # Load the .npy file
+        image = np.load(img_path)
+        
+        #image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+
+        # Adds randomly selected gauss noise or blur
+        if self.gauss:
+            # Gaussian Noise
+            gauss_noise = image + np.random.normal(loc=0, scale=random.choice(range(10,40)), size=image.shape)
+            # Gaussian Blur
+            gauss_blur = gaussian_filter(image, sigma=(random.choice(range(10,16))/10)) 
+            # Random choice
+            image = random.choice((gauss_noise,gauss_blur))
+
+        # Guarantee compatibility
+        if self.gauss or self.albumentations: image = image.astype(np.float32)
+
+        # Applies necessary ResNet input transformations
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        
+        return image, label, patient_id
+    
+
+# ------------------------- Main Training Loop ------------------------- #
+
+
+def trainResNet(train_dataset, val_dataset, num_epochs=90, batch_size=32, lr=5e-7, patience=5, improve_min=0.001):
+    # New ResNet instance
+    resnet18 = models.resnet18(weights=ResNet18_Weights.DEFAULT)
+
+    # Considers only absolute lowest val_loss value
+    best_model_pure = copy.deepcopy(resnet18)
+    best_val_pure, epoch_pure = 10, 0
+
+    # Use only 2 output neurons
+    num_ftrs = resnet18.fc.in_features
+    resnet18.fc = nn.Linear(num_ftrs, 2)  
+
+    # If graphics card is available, uses it
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    resnet18.to(device)
+
+    print("*"+("-"*29)+"*")
+    print("|{:^29}|".format(f"Using {device}"))
+    print("*"+("-"*29)+"*")
+
+    # Generate data loaders
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    
+    # Loss function 
+    criterion = nn.CrossEntropyLoss()  
+    # ADAM
+    optimizer = torch.optim.Adam(resnet18.parameters(), lr=lr)
+
+    # add learning_rate_scheduler e step learning rate
+
+    # Initializing early stop counter (based on validation loss)
+    best_loss = 10
+    early_stop_ctr = 0
+
+    loss_array = []
+    val_loss_array = []
+
+
+    for epoch in range(num_epochs):
+        resnet18.train()
+        running_loss = 0.0
+        running_val_loss = 0.0
+
+
+        # --------------- Weight updates ---------------
+        
+        # Iterates over each image in dataset, updates weights
+        for images, labels, patient_id in tqdm((train_loader), desc = "Training..."):
+            images, labels = images.to(device), labels.to(device)
+            
+            # Applies ADAM 
+            optimizer.zero_grad()
+
+            # Generates output
+            outputs = resnet18(images)
+
+            # Computes loss
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            
+            running_loss += loss.item()
+
+        # Computes average training loss
+        avg_loss_train = running_loss/len(train_loader)
+        print(f"Epoch [{epoch+1}/{num_epochs}], Train loss: {avg_loss_train:.6f}")
+        loss_array.append(avg_loss_train)
+
+
+        # ---------------- Validation Loss -----------------
+
+        # Iterates over each image in dataset, computes validation loss
+        for images, labels, patient_id in (val_loader):
+            images, labels = images.to(device), labels.to(device)
+            
+            # Applies ADAM 
+            optimizer.zero_grad()
+
+            # Generates output
+            outputs = resnet18(images)
+
+            # Computes loss
+            val_loss = criterion(outputs, labels)
+            
+            running_val_loss += val_loss.item()
+
+        # Computes average validation loss
+        avg_loss_val = running_val_loss/len(val_loader)
+        print(f"Validation loss: {avg_loss_val:.6f}")
+        val_loss_array.append(avg_loss_val)
+
+
+        # ------------------ Best Model -------------------
+
+        if epoch >= 35:
+            if avg_loss_val < best_val_pure:
+                best_val_pure = avg_loss_val
+                best_model_pure = copy.deepcopy(resnet18)
+                epoch_pure = epoch
+        
+        
+        # ---------------- Early Stopping -----------------
+
+        # Only after a high number of epochs (around 60)
+        if epoch > 60:
+            # Check for improvement (1% standard)
+            if abs(best_loss - avg_loss_val) > (best_loss * improve_min):
+                best_loss = avg_loss_val
+                early_stop_ctr = 0
+            else: early_stop_ctr += 1
+
+            # Checks early stopping condition
+            if early_stop_ctr >= patience:
+                print(f"Early stopping triggered after {epoch+1} epochs!!!")
+                break
+    
+    return best_model_pure, epoch_pure, loss_array, val_loss_array    
+
+
+# ------------------------- Individual Slice Evaluation ------------------------- #
+
+
+def evalResNet(resnet18, test_dataset, threshold):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+    # Counters for each class
+    correct, total = 0, 0
+    correct_class_0, total_class_0 = 0, 0
+    correct_class_1, total_class_1 = 0, 0
+
+    # Needed for F1 score and confusion matrix
+    y_true = []
+    y_pred = []
+
+    # for feature based
+    #lat=resnet.define_hook(fc)
+
+    with torch.no_grad():
+        for images, labels, patient_id in test_loader:
+            images, labels = images.to(device), labels.to(device)
+
+            # Transform outputs to probabilities
+            outputs = resnet18(images)
+
+            # Translate logits to Class 1 probabilities using softmax
+            probs = F.softmax(outputs, dim=1)[:, 1] 
+
+            # Translate prob vs threshold to predictions
+            # True if prob >= threshold else False
+            # .long() transforms True/False to 1/0
+            predicted = (probs >= threshold).long()
+            
+            # Update total and correct for general accuracy
+            total += labels.size(0)
+
+            correct += (predicted == labels).sum().item()
+
+            # Class-specific accuracy
+            for label, pred in zip(labels, predicted):
+                if label == 0:
+                    total_class_0 += 1
+                    if pred == label:
+                        correct_class_0 += 1
+                elif label == 1:
+                    total_class_1 += 1
+                    if pred == label:
+                        correct_class_1 += 1
+
+            # Add data to lists
+            y_true.extend(labels.cpu().tolist())
+            y_pred.extend(predicted.cpu().tolist())
+
+
+
+    # Weights == inverse proportions
+    weight_0, weight_1 = 0.133, 0.867
+
+    print("Total examples:", total)
+
+    # -------------------------------     Perfomance Metrics     -------------------------------
+
+    # Accuracy
+    accuracy_class_0 = 100 * (correct_class_0 / total_class_0) if total_class_0 > 0 else 0
+    accuracy_class_1 = 100 * (correct_class_1 / total_class_1) if total_class_1 > 0 else 0
+    accuracy = 100 * (correct / total)
+    accuracy2 = 100 * ((correct_class_0 + correct_class_1) / total)
+    weighted_accuracy = ((accuracy_class_0*weight_0) + (accuracy_class_1*weight_1))
+
+    # F1 scores
+    f1_macro = f1_score(y_true, y_pred, average='macro') # Assigns same importance to each class
+    f1_weighted = f1_score(y_true, y_pred, average='weighted')
+    f1_class_0 = f1_score(y_true, y_pred, pos_label=0)
+    f1_class_1 = f1_score(y_true, y_pred, pos_label=1)
+
+    # Confusion Matrix
+    conf_mat = confusion_matrix(y_true, y_pred)
+
+
+    # -------------------------------     Print Results     -------------------------------
+
+    # Accuracy
+    print("\n --------------------- \n")
+    print(f"Accuracy for Class 0: {accuracy_class_0:.2f}%  ({correct_class_0} in {total_class_0})")
+    print(f"Accuracy for Class 1: {accuracy_class_1:.2f}%  ({correct_class_1} in {total_class_1})")
+    print(f"Test Accuracy: {accuracy:.2f}%")
+    if f"{accuracy:.2f}" != f"{accuracy2:.2f}": print("ERROR CALCULATING ACCURACIES")
+    print(f"Weighted Accuracy: {weighted_accuracy:.2f}%")
+
+
+    # F1 scores
+    print("\n --------------------- \n")
+    print(f"F1 Score (Macro): {f1_macro:.3f}")
+    print(f"F1 Score (Weighted): {f1_weighted:.3f}")
+    print(f"F1 Score Class 0: {f1_class_0:.3f}")
+    print(f"F1 Score Class 1: {f1_class_1:.3f}")
+
+    # Confusion matrix
+    print("\n --------------------- \n")
+    print("\nConfusion Matrix: \n", conf_mat)
+
+
+# ------------------------- Patient-Wise Classification (Probabilities) ------------------------- #
+
+def evalPatientProbResNet(resnet18, test_dataset, threshold, aggregate_criteria="mean", n=1, class_condition="1_is_enough", verbose=True):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+    # Counters for each class
+    correct, total = 0, 0
+    correct_class_0, total_class_0 = 0, 0
+    correct_class_1, total_class_1 = 0, 0
+
+    # Needed for F1 score and confusion matrix
+    y_true = []
+    y_pred = []
+
+    # Needed for patient-wise classification
+    patient_prob = {}
+    patient_class = {}
+
+    # Used to store values after aggregation
+    patient_aggregate = {}
+
+
+    # 1. This loop populates dictionaries with values pulled from the model
+    with torch.no_grad():
+        for images, labels, patient_id in test_loader:
+            images, labels = images.to(device), labels.to(device)
+
+            # Transform outputs to probabilities
+            outputs = resnet18(images)
+
+            # Translate logits to Class 1 probabilities using softmax
+            probs = F.softmax(outputs, dim=1)[:, 1] 
+
+            # Associate probabilities to dictionary for each patient
+            for pid, prob, label in zip(patient_id, probs.tolist(), labels.tolist()):
+                if pid not in patient_prob:
+                    patient_prob[pid] = []  # Initializes key:value pair if it doesn't exist
+                    patient_class[pid] = 0  # Sets classification as 0 until otherwise
+
+                patient_prob[pid].append(prob)  # Adds probability to list
+                
+                # Label aggregation condition
+                if class_condition == "1_is_enough":
+                    if label == 1: patient_class[pid] = 1 # Updates Classification
+    
+    # 2. After pulling every slice value for every patient, apply aggregate_criteria
+    # and evaluate the aggregated values using criteria-specific conditions
+    for id, prob_list in patient_prob.items():
+        if aggregate_criteria == "mean":
+            prob = bool(np.mean(prob_list))
+            patient_aggregate[id] = prob
+
+            # If larger -> True -> int -> 1
+            # If smaller -> False -> int -> 0
+            predicted = int(prob >= threshold)
+        
+        elif aggregate_criteria == "majority_vote":
+            ctr0, ctr1 = 0, 0
+            for prob in prob_list:
+                predicted = int(prob >= threshold)
+                if predicted == 1: ctr1 += 1
+                elif predicted == 0: ctr0 += 1
+                else: print("Error: Invalid prediction for patient", id)
+            
+            # From a medical standpoint, I decided 50/50 calls for more
+            # tests, as a prevention for false negatives in these cases
+            if (ctr1 >= ctr0): predicted == 1
+            else: predicted == 0
+
+        elif aggregate_criteria == "n_is_enough":
+            n_ctr = 0
+
+            # Absolute slice number
+            if isinstance(n, int):
+                for prob in prob_list:
+                    predicted = int(prob >= threshold)
+                    if predicted == 1: n_ctr += 1
+                    elif predicted != 0: print("Error: Invalid prediction for patient", id)
+
+                    # Early stopping
+                    if n_ctr == n: break
+
+                # Assigning correct values
+                if n_ctr >= n: predicted = 1
+                else: predicted = 0
+
+            # Relative slice ammount
+            elif isinstance(n, float):
+                size = len(prob_list)
+                for prob in prob_list:
+                    predicted = int(prob >= threshold)
+                    if predicted == 1: n_ctr += 1
+                    elif predicted != 0: print("Error: Invalid prediction for patient", id)
+
+                    # Early stopping
+                    if n_ctr >= n * size: break
+
+                # Assigning correct values
+                if n_ctr >= n * size: predicted = 1
+                else: predicted = 0
+
+                #if predicted == 1: print(" +++++ ", id, " | ", n_ctr, "out of", size, "slices!")
+                #else: print(" ----- ", id, " | ", n_ctr, "out of", size, "slices!")
+            
+            else: print("Error: Invalid n value for patient", id)
+
+                    
+
+        label = patient_class[id]
+
+        # Counters for metrics   
+        total += 1
+        if label == 0:
+            total_class_0 += 1
+            if predicted == label:
+                correct_class_0 += 1
+                correct += 1
+        elif label == 1:
+            total_class_1 += 1
+            if predicted == label:
+                correct_class_1 += 1
+                correct += 1
+
+        # Populate y_true and y_pred lists for F1 Score
+        y_true.append(label)
+        y_pred.append(predicted)
+        
+
+    
+    # Weights == inverse proportions
+    weight_0, weight_1 = 0.133, 0.867
+
+    print("Total examples:", total)
+
+    # -------------------------------     Perfomance Metrics     -------------------------------
+
+    # Accuracy
+    accuracy_class_0 = 100 * (correct_class_0 / total_class_0) if total_class_0 > 0 else 0
+    accuracy_class_1 = 100 * (correct_class_1 / total_class_1) if total_class_1 > 0 else 0
+    accuracy = 100 * (correct / total)
+    accuracy2 = 100 * ((correct_class_0 + correct_class_1) / total)
+    weighted_accuracy = ((accuracy_class_0*weight_0) + (accuracy_class_1*weight_1))
+
+    # F1 scores
+    f1_macro = f1_score(y_true, y_pred, average='macro') # Assigns same importance to each class
+    f1_weighted = f1_score(y_true, y_pred, average='weighted')
+    f1_class_0 = f1_score(y_true, y_pred, pos_label=0)
+    f1_class_1 = f1_score(y_true, y_pred, pos_label=1)
+
+    # Confusion Matrix
+    conf_mat = confusion_matrix(y_true, y_pred)
+
+
+    # -------------------------------     Print Results     -------------------------------
+
+    if verbose:
+
+        # Accuracy
+        print("\n --------------------- \n")
+        print(f"Accuracy for Class 0: {accuracy_class_0:.2f}%  ({correct_class_0} in {total_class_0})")
+        print(f"Accuracy for Class 1: {accuracy_class_1:.2f}%  ({correct_class_1} in {total_class_1})")
+        print(f"Test Accuracy: {accuracy:.2f}%")
+        if f"{accuracy:.2f}" != f"{accuracy2:.2f}": print("ERROR CALCULATING ACCURACIES")
+        print(f"Weighted Accuracy: {weighted_accuracy:.2f}%")
+
+        # F1 scores
+        print("\n --------------------- \n")
+        print(f"F1 Score (Macro): {f1_macro:.3f}")
+        print(f"F1 Score (Weighted): {f1_weighted:.3f}")
+        print(f"F1 Score Class 0: {f1_class_0:.3f}")
+        print(f"F1 Score Class 1: {f1_class_1:.3f}")
+
+        # Confusion matrix
+        print("\n --------------------- \n")
+        print("\nConfusion Matrix: \n", conf_mat)    
+
 
 # ------------------------------------------------ #
 
@@ -116,6 +570,7 @@ def tensorVSnp(dataset, df_fibrosis, rgb=True):
     plt.show()
 
 
+# ---------------------------------------------------- #
 
 
 def getROC(model, val_dataset): 
@@ -168,6 +623,8 @@ def getROC(model, val_dataset):
 
     return best_threshold, roc_auc
 
+# ---------------------------------------------------- #
+
 
 def plotLoss(train_loss, val_loss, best_epoch):
     epochs = range(1, len(train_loss) + 1)
@@ -186,3 +643,19 @@ def plotLoss(train_loss, val_loss, best_epoch):
     plt.grid(True)
     plt.tight_layout()
     plt.show()
+
+# ---------------------------------------------------- #
+
+
+def getPatientID(s):
+    # HRCT_Pilot__PatientID
+    match = re.search(r'HRCT_Pilot__(\d+)__', s)
+    if match: return match.group(1)
+        
+    # PatientID__sliceSpecific
+    match = re.match(r'(\d+)__', s)
+    if match:
+        return match.group(1)
+    return None  
+
+
